7972561d8272b25e759b3d21e9a677f264c34639
Ben Nordick <bnordick@vols.utk.edu>
2020-08-22 23:44:17 -0400

    Permute regulation types

--- permutenetwork.py
+++ permutenetwork.py
@@ -2,6 +2,7 @@ import networkx as nx
 import random
 
 def permutenetwork(graph, swaps):
+    '''Randomly moves regulations, preserving degree sequence.'''
     if len(graph.edges) < 2:
         raise ValueError("graph must have at least two edges")
     if not nx.algorithms.components.is_strongly_connected(graph):
@@ -24,9 +25,7 @@ def edgeswap(graph):
 
 def tryedgeswap(graph):
     edges = list(graph.edges)
-    ab, cd = random.choices(edges, k=2)
-    if (ab == cd):
-        return False
+    ab, cd = random.sample(edges, 2)
     a, b = ab
     c, d = cd
     if graph.has_edge(a, d) or graph.has_edge(c, b):
@@ -43,3 +42,17 @@ def createedge(graph, src, dst, data):
     graph.add_edge(src, dst)
     for k, v in data.items():
         graph.edges[src, dst][k] = v
+
+def permuteregulations(graph):
+    '''Randomly changes which regulations are repressions, maintaining activation and repression counts and directions.'''
+    edges = list(graph.edges)
+    copy = graph.copy()
+    repressions = 0
+    for edge in edges:
+        edge_data = copy.edges[edge]
+        if edge_data['repress']:
+            repressions += 1
+            edge_data['repress'] = False
+    for new_repression in random.sample(edges, repressions):
+        copy.edges[new_repression]['repress'] = True
+    return copy
