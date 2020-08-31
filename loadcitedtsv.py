c5c082af1d838a1608535c2bc43f30b244804fd2
Ben Nordick <bnordick@vols.utk.edu>
2020-08-30 22:42:37 -0400

    Load TRRUSTv2 data

---
+++ loadcitedtsv.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+import networkx as nx
+import sys
+
+def loadcitedtsv(reader):
+    evidence = defaultdict(int)
+    directions = {'Activation': 1, 'Repression': -1, 'Unknown': 0}
+    graph = nx.DiGraph()
+    for line in reader:
+        tf, target, direction, refs = line.rstrip().split('\t')
+        evidence[tf, target] += directions[direction] * (refs.count(';') + 1)
+        graph.add_node(tf)
+        graph.add_node(target)
+    for (tf, target), balance in evidence.items():
+        if balance == 0:
+            continue
+        graph.add_edge(tf, target, repress=(balance < 0))
+    for node in graph.nodes:
+        graph.nodes[node]['name'] = node
+    return nx.convert_node_labels_to_integers(graph)
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as f:
+        network = loadcitedtsv(f)
+        nx.write_graphml(network, sys.argv[2])
