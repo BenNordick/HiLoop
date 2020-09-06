8a9442ac23cdc477369ad128bf4de4e73105b384
Ben Nordick <bnordick@vols.utk.edu>
2020-09-05 20:50:07 -0400

    Allow limiting node sharing between examples

--- examplemotifs.py
+++ examplemotifs.py
@@ -19,10 +19,12 @@ parser.add_argument('count1', type=int, help='how many Type I examples to find')
 parser.add_argument('count2', type=int, help='how many Type II examples to find')
 parser.add_argument('--maxedges', type=int, help='maximum number of unique edges in examples')
 parser.add_argument('--maxnodes', type=int, help='maximum size of network to attempt cycle detection for')
+parser.add_argument('--maxsharing', type=int, help='maximum number of nodes in common with an already drawn subnetwork')
 args = parser.parse_args()
 
 graph = nx.convert_node_labels_to_integers(nx.read_graphml(args.file))
 type1, type2 = 0, 0
+seen = []
 while type1 < args.count1 or type2 < args.count2:
     feasible = graph if args.maxnodes is None else permutenetwork.randomsubgraph(graph, args.maxnodes)
     cycles = [(cycle, ispositive(feasible, cycle)) for cycle in nx.algorithms.simple_cycles(feasible)]
@@ -34,6 +36,14 @@ while type1 < args.count1 or type2 < args.count2:
     chosen_cycles = [entry[0] for entry in chosen]
     cycle_sets = [frozenset(cycle) for cycle in chosen_cycles]
     used_nodes = cycle_sets[0].union(cycle_sets[1]).union(cycle_sets[2])
+    if args.maxsharing is not None:
+        duplicate = False
+        for ns in seen:
+            if len(ns.intersection(used_nodes)) > args.maxsharing:
+                duplicate = True
+                break
+        if duplicate:
+            continue
     subgraph = graph.subgraph(used_nodes)
     if args.maxedges is not None and len(subgraph.edges) > args.maxedges:
         continue
@@ -45,6 +55,7 @@ while type1 < args.count1 or type2 < args.count2:
                 colored.nodes[n]['penwidth'] = 2.0
             type1 += 1
             rendergraph.rendergraph(colored, args.output.format(type1, len(colored.edges), 1), in_place=True)
+            seen.append(used_nodes)
             continue
     if type2 < args.count2:
         for connector_c, connector in enumerate(cycle_sets):
@@ -56,5 +67,6 @@ while type1 < args.count1 or type2 < args.count2:
                 colored = colorsubgraph(subgraph, chosen_cycles[(connector_c + 1) % 3], chosen_cycles[connector_c], chosen_cycles[(connector_c + 2) % 3])
                 type2 += 1
                 rendergraph.rendergraph(colored, args.output.format(type2, len(colored.edges), 2), in_place=True)
+                seen.append(used_nodes)
                 break
     
