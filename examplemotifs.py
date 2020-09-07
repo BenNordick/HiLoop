84e7bae29fd5e781cc0fb677ea0accfdb4c8387e
Ben Nordick <bnordick@vols.utk.edu>
2020-09-07 19:32:34 -0400

    Allow printing nodes involved in example motifs

--- examplemotifs.py
+++ examplemotifs.py
@@ -10,13 +10,14 @@ def colorsubgraph(graph, r, y, b):
     rendergraph.colorcycle(colored, r, 'red', {})
     rendergraph.colorcycle(colored, y, 'yellow', 'orange')
     rendergraph.colorcycle(colored, b, 'blue', {'red': 'purple', 'yellow': 'green', 'orange': 'gray50'})
-    return colored
+    return colored 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file', type=str, help='input GraphML file')
-parser.add_argument('output', type=str, help='output file pattern {id, edges, type}')
 parser.add_argument('count1', type=int, help='how many Type I examples to find')
 parser.add_argument('count2', type=int, help='how many Type II examples to find')
+parser.add_argument('--images', type=str, help='output file pattern {id, edges, type}')
+parser.add_argument('--printnodes', action='store_true', help='print distinct node names')
 parser.add_argument('--maxedges', type=int, help='maximum number of unique edges in examples')
 parser.add_argument('--maxnodes', type=int, help='maximum size of network to attempt cycle detection for')
 parser.add_argument('--maxsharing', type=int, help='maximum number of nodes in common with an already drawn subnetwork')
@@ -25,6 +26,15 @@ args = parser.parse_args()
 graph = nx.convert_node_labels_to_integers(nx.read_graphml(args.file))
 type1, type2 = 0, 0
 seen = []
+printed_nodes = set()
+
+def printnewnodes(nodes):
+    if not args.printnodes:
+        return
+    for n in nodes.difference(printed_nodes):
+        printed_nodes.add(n)
+        print(graph.nodes[n]['name'])
+
 while type1 < args.count1 or type2 < args.count2:
     feasible = graph if args.maxnodes is None else permutenetwork.randomsubgraph(graph, args.maxnodes)
     cycles = [(cycle, ispositive(feasible, cycle)) for cycle in nx.algorithms.simple_cycles(feasible)]
@@ -50,12 +60,14 @@ while type1 < args.count1 or type2 < args.count2:
     if type1 < args.count1:
         intersection = cycle_sets[0].intersection(cycle_sets[1]).intersection(cycle_sets[2])
         if len(intersection) > 0:
-            colored = colorsubgraph(subgraph, *chosen_cycles)
-            for n in intersection:
-                colored.nodes[n]['penwidth'] = 2.0
-            type1 += 1
-            rendergraph.rendergraph(colored, args.output.format(type1, len(colored.edges), 1), in_place=True)
             seen.append(used_nodes)
+            type1 += 1
+            printnewnodes(used_nodes)
+            if args.images:
+                colored = colorsubgraph(subgraph, *chosen_cycles)
+                for n in intersection:
+                    colored.nodes[n]['penwidth'] = 2.0
+                rendergraph.rendergraph(colored, args.images.format(type1, len(colored.edges), 1), in_place=True)
             continue
     if type2 < args.count2:
         for connector_c, connector in enumerate(cycle_sets):
@@ -64,9 +76,11 @@ while type1 < args.count1 or type2 < args.count2:
             if connector.isdisjoint(other1) or connector.isdisjoint(other2):
                 continue
             if other1.isdisjoint(other2):
-                colored = colorsubgraph(subgraph, chosen_cycles[(connector_c + 1) % 3], chosen_cycles[connector_c], chosen_cycles[(connector_c + 2) % 3])
-                type2 += 1
-                rendergraph.rendergraph(colored, args.output.format(type2, len(colored.edges), 2), in_place=True)
                 seen.append(used_nodes)
+                type2 += 1
+                printnewnodes(used_nodes)
+                if args.images:
+                    colored = colorsubgraph(subgraph, chosen_cycles[(connector_c + 1) % 3], chosen_cycles[connector_c], chosen_cycles[(connector_c + 2) % 3])
+                    rendergraph.rendergraph(colored, args.images.format(type2, len(colored.edges), 2), in_place=True)
                 break
     
