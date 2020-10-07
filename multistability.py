621c2bb87ab54141647cee048de82e78ba40ab3d
Ben Nordick <bnordick@vols.utk.edu>
2020-10-06 20:20:16 -0400

    Try parallelizing findmultistability

--- multistability.py
+++ multistability.py
@@ -1,4 +1,5 @@
 import argparse
+import concurrent
 from itertools import product
 import json
 import networkx as nx
@@ -6,46 +7,59 @@ import numpy as np
 import tellurium as te
 
 # Adapted from a script written by Tian Hong 9/21/2020
-def findmultistability(runner, n_pts1d=5, n_psets=1000, min_attractors=2, time=50, dt=5, print_results=False):
+def findmultistability(runner_factory, n_pts1d=5, n_psets=1000, min_attractors=2, time=250, dt=5, threads=4, print_results=False):
+
+    def run_one(runner):
+        runner.simulate(start=0, end=time, points=round(time / dt))
+        return runner.sv() # Get the steady state solution. Note: the variable names (unsorted) are r.fs()
 
     # Define initial conditions for simulations
     # A uniform grid is used. When number of genes is large, consider using Latin Hypercube Sampling
-    n_genes = runner.sv().size
+    example_runner = runner_factory()
+    n_genes = example_runner.sv().size
     pts1d = np.linspace(0.0, 3.3, n_pts1d)
     ptsnd = np.tile(pts1d, n_genes).reshape(n_genes, -1)
     ini_combs = np.array(list(product(*ptsnd))) # all combinations of initial conditions
 
     # Ranges of parameter values to sample
     doms = {'K': (0.05, 4.5), 'k': [3.0, 3.3], 'r': [0.9, 0.99], 'n': [1, 6]}
-    rands = np.random.uniform(size=(n_psets, len(runner.ps()))) # Random numbers for perturbing parameters
+    rands = np.random.uniform(size=(n_psets, len(example_runner.ps()))) # Random numbers for perturbing parameters
 
-    results = {'species_names': runner.fs(), 'parameter_names': runner.ps(), 'psets': []}
-    for i in range(n_psets):
-        for ip, p in enumerate(runner.ps()):
-            dom = doms[p[0]]
-            runner[p] = dom[0] + rands[i, ip] * (dom[1] - dom[0]) # Set parameter value
-            if p[0] == 'n':
-                runner[p] = np.round(runner[p], decimals=0) # n is usually an integer
-        sols = None
-        for ii, ini_comb in enumerate(ini_combs):
-            for iv, v in enumerate(runner.fs()):
-                runner[v] = ini_comb[iv] # Set initial condition
-            runner.simulate(start=0, end=time, points=round(time / dt))
-            ss = runner.sv() # Get the steady state solution. Note: the variable names (unsorted) are r.fs()
-            if sols is None:
-                sols = ss
-            else:
-                if len(sols.shape) > 1:
-                    dist = np.linalg.norm(sols - ss, axis=1)
+    runners = [runner_factory() for _ in range(len(ini_combs) - 1)]
+    runners.append(example_runner)
+
+    results = {'species_names': example_runner.fs(), 'parameter_names': example_runner.ps(), 'psets': []}
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+        for i in range(n_psets):
+            for ip, p in enumerate(example_runner.ps()):
+                dom = doms[p[0]]
+                value = dom[0] + rands[i, ip] * (dom[1] - dom[0]) # Parameter value
+                if p[0] == 'n':
+                    value = np.round(value, decimals=0) # n is usually an integer
+                for runner in runners:
+                    runner[p] = value
+            futures = []
+            for ii, ini_comb in enumerate(ini_combs):
+                for iv, v in enumerate(example_runner.fs()):
+                    runners[ii][v] = ini_comb[iv] # Set initial condition
+                futures.append(executor.submit(run_one, runners[ii]))
+            sols = None
+            for future in futures:
+                ss = future.result()
+                if sols is None:
+                    sols = ss
                 else:
-                    dist = np.linalg.norm(sols - ss)
-                if not np.any(dist < 5E-1): # Make sure we only save non-redundant steady states
-                    sols = np.vstack((sols, ss))
-        if len(sols.shape) > 1 and sols.shape[0] >= min_attractors: # Multiple attractors with a single parameter set
-            result = {'parameters': [runner[p] for p in runner.ps()], 'attractors': [list(state) for state in sols]}
-            results['psets'].append(result)
-            if print_results:
-                print(sols, f'(pset {i})')
+                    if len(sols.shape) > 1:
+                        dist = np.linalg.norm(sols - ss, axis=1)
+                    else:
+                        dist = np.linalg.norm(sols - ss)
+                    if not np.any(dist < 5E-1): # Make sure we only save non-redundant steady states
+                        sols = np.vstack((sols, ss))
+            if len(sols.shape) > 1 and sols.shape[0] >= min_attractors: # Multiple attractors with a single parameter set
+                result = {'parameters': [example_runner[p] for p in example_runner.ps()], 'attractors': [list(state) for state in sols]}
+                results['psets'].append(result)
+                if print_results:
+                    print(sols, f'(pset {i})')
     return results
 
 def networksb(network):
@@ -68,9 +82,10 @@ def networksb(network):
         parts.append(f'\nK_{interaction_id} = 1.0; n_{interaction_id} = 4;')
     return ''.join(parts)
     
-def networkmodel(network):
-    '''Converts a NetworkX directed graph to a Tellurium model.'''
-    return te.loada(networksb(network))
+def networkmodelfactory(network):
+    '''Converts a NetworkX directed graph to a factory of Tellurium models.'''
+    sb = networksb(network)
+    return lambda: te.loada(sb)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -78,14 +93,16 @@ if __name__ == "__main__":
     parser.add_argument('output', type=str, help='output JSON file path')
     parser.add_argument('--psets', type=int, default=1000, help='number of parameter sets to try')
     parser.add_argument('--attractors', type=int, default=2, help='minimum number of attractors to report multistability')
-    parser.add_argument('--time', type=int, default=50, help='length of simulation')
+    parser.add_argument('--time', type=int, default=250, help='length of simulation')
     parser.add_argument('--dt', type=float, default=5.0, help='time step length for runner.simulate')
+    parser.add_argument('--cpus', type=int, default=4, help='number of threads to use')
     args = parser.parse_args()
     if args.input.endswith('.sb'):
         with open(args.input) as f:
-            r = te.loada(f.read())
+            sb = f.read()
+            r = lambda: te.loada(sb)
     else:
-        r = networkmodel(nx.read_graphml(args.input))
-    result = findmultistability(r, n_psets=args.psets, min_attractors=args.attractors, time=args.time, dt=args.dt, print_results=True)
+        r = networkmodelfactory(nx.read_graphml(args.input))
+    result = findmultistability(r, n_psets=args.psets, min_attractors=args.attractors, time=args.time, dt=args.dt, threads=args.cpus, print_results=True)
     with open(args.output, 'w') as f:
         f.write(json.dumps(result))
